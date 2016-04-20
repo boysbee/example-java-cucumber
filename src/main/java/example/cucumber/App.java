e1eba2ea417d299c48076cf0af58c6907440680a
nattaporn.c <gogorenger@gmail.com>
2016-04-21 01:36:18 +0700

    renew structure project.

---
+++ src/main/java/example/cucumber/App.java
@@ -0,0 +1,10 @@
+package example.cucumber;
+
+/**
+ * Created by boysbee on 4/21/2016.
+ */
+public class App {
+    public static void main(String [] args){
+        System.out.println("Hello.");
+    }
+}
---
+++ src/test/java/SomeTest.java
@@ -0,0 +1,9 @@
+
+import cucumber.api.junit.Cucumber;
+import org.junit.runner.RunWith;
+/**
+ * Created by boysbee on 4/21/2016.
+ */
+@RunWith(Cucumber.class)
+public class SomeTest {
+}
